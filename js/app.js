a3ae0941d55d204498987642ecc9af81a494fffe
Emeka Onyenwe <emeka.onyenwe@gmail.com>
2019-02-11 17:58:56 +0100

    Add function to initialize new game

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ let card = document.getElementsByClassName('card');
 const cards = [...card];
 
 //variable that holds all the cards
-const cardDeck =document.getElementByID('deck');
+const cardDeck = document.getElementById('deck');
 
 //variable to count moves
-let moves =0;
+let moves = 0;
 let count = document.querySelector('.moves');
 
 // variable to store stars list
@@ -20,6 +20,7 @@ let matchedCards = document.getElementsByClassName('match');
 
 //variable for modal
 let modal = document.getElementByID('modal');
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -34,6 +35,36 @@ function shuffle(array) {
     }
 
     return array;
+}
+ 
+
+// Initializing Start game function when browser window is loaded
+
+window.onload = startGame();
+function startGame() {
+        //a new deck of cards is shuffled for every game
+    let shuffleCards = shuffle(cards);
+    for (let i = 0; i < shuffleCards.length; i++) {
+        cardDeck.innerHTML = "";
+        [].forEach.call(cards, function(item){
+            cardDeck.appendChild(item);
+        });
+        cards[i].classList.remove("show", "open", "match", "disabled");
+    }
+        // moves is set to 0
+    moves = 0;
+    counter.innerHTML = moves;
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].style.color = "FFD700";
+        stars[i].style.visibility = "visible";
+    }
+        //timer is set to 0
+    second = 0;
+    minute = 0;
+    hour = 0;
+    let timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
 }
 
 
