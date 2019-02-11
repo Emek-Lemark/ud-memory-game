da84620f0f7b66b5c42be1408dcc82600b08c9fe
Emeka Onyenwe <emeka.onyenwe@gmail.com>
2019-02-11 12:12:42 +0100

    Restarting experiment to use Vanilla JS

--- js/app.js
+++ js/app.js
@@ -1,35 +1,14 @@
+/*
+ * Create a list that holds all of your cards
+ */
 
- // Create array that holds all cards
-const cardDeck = ["fa-diamond", "fa-paper-plane-o", "fa fa-futbol-o" "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
-// Declare variable to store moves and matches
-let moves = 0;
-let matches = 0;
-
-// Set first card 
-let game_start = false;
-
-// Launch timer object
-let timer = new Timer();
-timer.addEventListener('secondsUpdated', function (e) {  
- document.querySelector('#timer').html(timer.getTimeValues().toString());
-});
-
-// Variable for reset
-let reset = document.querySelector('#reset-button').click(resetGame);
-
-//  Append created card html
-function createCard(card) {
-    document.querySelector('#deck').append(`<li class="card animated"><i class="fa ${card}"></i></li>`);
-}
-
-// Generate random cards on the deck
-function generateCards() {
-    for (let i = 0; i < 2; i++) {
-        cardDeck = shuffle(cardDeck);
-        cardDeck.forEach(createCard);
-    }
-}
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -57,15 +36,3 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-// add blank stars
-function addBlankStar() {
-    document.querySelector('#stars').children()[0].remove();
-    document.querySelector('#stars').append('<li><i class="fa fa-star-o"></i></li>');
-}
-// add initial stars
-function addStars() {
-    for (var i = 0; i < 3; i++) {
-        document.querySelector('#stars').append('<li><i class="fa fa-star"></i></li>');
-    }
-}
