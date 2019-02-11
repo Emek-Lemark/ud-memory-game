dbb7b8777c231f7c39b5736fa782302190a2202c
Emeka Onyenwe <emeka.onyenwe@gmail.com>
2019-02-11 17:43:45 +0100

    Set variables in JS for DOM manipulation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 
+//declare variables to store array in cards
 
+let card = document.getElementsByClassName('card');
+const cards = [...card];
+
+//variable that holds all the cards
+const cardDeck =document.getElementByID('deck');
+
+//variable to count moves
+let moves =0;
+let count = document.querySelector('.moves');
+
+// variable to store stars list
+const stars = document.querySelectorAll('.fa-star');
+let threeStars = document.querySelectorAll('.stars li');
+
+// variable for matched cards
+let matchedCards = document.getElementsByClassName('match');
+
+//variable for modal
+let modal = document.getElementByID('modal');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
